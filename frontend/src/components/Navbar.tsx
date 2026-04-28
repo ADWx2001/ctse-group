fb74087c1060bd96ba3229d7b12eff10f496eae3
IT22119230 <it2211930@my.sliit.lk>
2026-04-28 15:04:17 +0530

    front end changed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
               🍔
             </span>
             <span>
-              Food<span className="text-[#06C167]">Order System</span>
+              SLIIT Food<span className="text-[#06C167]">Order System</span>
               {isOwner && (
                 <span className="ml-2 text-xs font-normal text-[#06C167] border border-[#06C167] rounded-full px-2 py-0.5">
                   Owner
